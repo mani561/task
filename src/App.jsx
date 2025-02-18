69622f034900d7b65e36138592f683e05a1e76dd
mani561 <manikantareddy5171@gmail.com>
2025-02-18 15:37:44 +0530

    chges to mani

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Box from '@mui/material/Box';
+import Page from "./component/Page";
+
+
+
+  const App = () => {
+    return(
+     <Box>
+      <Page/>
+      </Box> 
+    )
+  }
+
+export default App
---
+++ src/component/Page/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import Navebar from '../Navebar'
+
+const Page = () => {
+  return (
+    
+      <Box>
+        <Navebar/>
+      </Box>
+    
+  )
+}
+
+export default Page
